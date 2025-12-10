2546843165f070a8a753e98498f57d9fa9555006
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-10 19:26:10 +0500

    ..

---
+++ src/LoginSignup/index.jsx
@@ -0,0 +1,75 @@
+import React, { useRef } from 'react'
+import './style.css'
+// import arpLogo from './assets/arpLogo.png'
+const LoginSignup = () => {
+  const signUpBox = useRef(null);
+  const logInBox = useRef(null);
+  const accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+  // const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+
+  const usernameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const handleSignUp = () => {
+    const username = usernameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const userData = { username, email, password, id: Date.now(), tasks: [] };
+    accounts.push(userData);
+    localStorage.setItem('accounts', JSON.stringify(accounts));
+    console.log(JSON.parse(localStorage.getItem('accounts')));
+    usernameRef.current.value = '';
+    emailRef.current.value = '';
+    passwordRef.current.value = '';
+
+    signUpBox.current.style.display = 'none';
+  }
+
+  const liEmailRef = useRef(null);
+  const liPasswordRef = useRef(null);
+
+  const handleLogIn = () => {
+    const email = liEmailRef.current.value;
+    const password = liPasswordRef.current.value;
+    const account = accounts.find(acc => acc.email === email && acc.password === password);
+
+
+    if (account) {
+      alert('Login successful');
+      localStorage.setItem('loggedInUser', JSON.stringify(account));
+    } else {
+      alert('Invalid email or password');
+    }
+  }
+
+  return (
+    <>
+      <span className='container'>
+        {/* <img src={arpLogo} alt="ARP Logo" /> */}
+        <span className='signUp' ref={signUpBox}>
+          <h2>Sign Up</h2>
+          <input type="text" placeholder='Username' ref={usernameRef} />
+          <input type="email" placeholder='Email' ref={emailRef} />
+          <input type="password" placeholder='Password' ref={passwordRef} />
+          <button onClick={handleSignUp}>Sign Up</button>
+          <button onClick={() => {
+            signUpBox.current.style.display = 'none';
+            logInBox.current.style.display = 'block';
+          }}>Already have an account? Log In</button>
+        </span>
+        <span className='logIn' ref={logInBox}>
+          <h2>Log In</h2>
+          <input type="email" placeholder='Email' ref={liEmailRef} />
+          <input type="password" placeholder='Password' ref={liPasswordRef} />
+          <button onClick={handleLogIn}>Log In</button>
+          <button onClick={() => {
+            logInBox.current.style.display = 'none';
+            signUpBox.current.style.display = 'block';
+          }}>Dont have an account? Sign Up</button>
+        </span>
+      </span>
+    </>
+  )
+}
+
+export default LoginSignup;
---
+++ src/profile/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => {
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+  return (
+    <div>
+        <h1>Profile</h1>
+        <p>Username: {loggedInUser?.username}</p>
+        <p>Email: {loggedInUser?.email}</p>
+    </div>
+  )
+}
+
+export default Profile
