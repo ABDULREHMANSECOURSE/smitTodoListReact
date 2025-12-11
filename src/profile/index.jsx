9807370fcc864e27f4a9fec54c9cd25164609f96
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-11 20:28:48 +0500

    .

--- src/profile/index.jsx
+++ src/profile/index.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
     const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
     if (!loggedInUser) {
         window.location.href = '/login';
     }
-  return (
-    <div>
-        <h1>Profile</h1>
-        <p>Username: {loggedInUser?.username}</p>
-        <p>Email: {loggedInUser?.email}</p>
-        <button onClick={() => {
-            window.location.href = '/';
-        }}>Back to Todo List</button>
-        <button onClick={() => {
-            localStorage.removeItem('loggedInUser');
-            window.location.href = '/login';
-        }}>Logout</button>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>Username: {loggedInUser?.username}</p>
+            <p>Email: {loggedInUser?.email}</p>
+            <button><Link to="/">Back to Todo List</Link></button>
+            <button><Link to="/login" onClick={() => {
+                localStorage.removeItem('loggedInUser');
+            }}>Logout</Link></button>
+        </div>
+    )
 }
 
 export default Profile
